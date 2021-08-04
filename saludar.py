6374b5379470b4eafae6fd2612b63cecdf062f3d
CosySE <79483122+CosySE@users.noreply.github.com>
2021-08-04 09:50:23 -0400

    Update saludar.py

--- saludar.py
+++ saludar.py
@@ -4,7 +4,7 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.image import Image
-
+#sencilla aplicacion en kivy introduces un nombre y te saluda la presionar el boton espero les ayude
 class Saludar (App):
     def build(self):
         self.ventana = GridLayout()
